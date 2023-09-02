cf0afc46024c2f54e2eb542eb7bf2064ff69e884
schuke <xk233qwq@163.com>
2023-09-02 17:54:19 +0800

    2023/09/02 基础结构搭建

---
+++ .idea/fileTemplates/internal/Enum.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+/**
+ * 
+ * @author 一只枫兔
+ * @Project ${PROJECT_NAME}
+ * @Module
+ * @Date ${DATE} ${TIME}
+ */
+public enum ${NAME}
+{
+}
